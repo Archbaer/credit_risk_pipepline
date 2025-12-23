ac5ea0fb4fc9b136d6725a552a57729db2cb236b
archbaer <pereirakebaili@gmail.com>
2025-12-23 22:34:03 +0000

    update: updated training.py to load and split data for the model, and also included a function to save the model parameters

--- models/training.py
+++ models/training.py
@@ -1,3 +1,47 @@
 from sklearn import train_test_split
 from pipelines.__init__ import logger
+from pathlib import Path
+from typing import Union 
+import pandas as pd
+import numpy as np
+from sklearn.metrics import classification_report, confusion_matrix
 import joblib
+
+def load_and_split(data_path: Union[str, Path], test_size=0.2, random_state=42):
+    """
+    Load data from a CSV file and split it into training and testing sets.
+
+    Args:
+        data_path (Union[str, Path]): The path to the CSV file containing the data.
+        test_size (float): The proportion of the dataset to include in the test split.
+        random_state (int): Random seed for reproducibility.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: The training and testing features and labels.
+    """
+    logger.info(f"Loading data from {data_path}")
+    data = pd.read_csv(data_path)
+    x = np.array(data.iloc[:, :-1])
+    y = np.array(data.iloc[:, -1])
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        x, y, test_size=test_size, random_state=random_state, stratify=y
+    )
+
+    logger.info(f"Data loaded and split from {data_path}")
+
+    return X_train, X_test, y_train, y_test
+
+def save_model(model, model_path: Union[str, Path]) -> None:
+    """Save the trained model to the specified path.
+
+    Args:
+        model: The trained model to be saved.
+        model_path (Union[str, Path]): The path where the model will be saved.
+    """
+    try:
+        joblib.dump(model, model_path)
+        logger.info(f"Model saved successfully at {model_path}")
+    except Exception as e:
+        logger.error(f"Failed to save model at {model_path}: {e}")
+        raise e
